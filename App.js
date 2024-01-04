d30f77731e203432d5e3e43cbc5266514cccc8b3
subha2004 <125337608+subha2004@users.noreply.github.com>
2024-01-04 10:51:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+
+// import React from 'react';
+// // import logo from './logo.svg';
+// import Login from './component/Login/Login.jsx';
+// //import Slid from './component/Slid';
+// import Signup from './component/Login/Signup';
+// import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+// import Navbar from './component/Navbar';
+// import About from './component/About';
+// import Contact from './component/Contact';
+// import Home from './component/Home';
+// const App = () =>{
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar/> 
+        
+//    <Routes>
+//     <Route path ="/Home" element = {<Home />}/>
+//     <Route path="/Login" element = {<Login/>}/>
+//     <Route path="/Signup" element ={<Signup/>}/>
+//     <Route path="/About" element ={<About/>}/>
+//     {/* <Route path="/Contact" element ={<Contact/>}/> */}
+//     <Route path="/Contact" element ={<Contact/>}/>
+    
+//    </Routes>
+//    </div>
+//    </Router>
+//   );
+// };
+//  export default App;
+import React from 'react'
+import Form from './component/Form'
+const App = () =>{
+   return(
+    <Form/>
+   )
+}
+export default App;
